d028f9aeff27c8ca60f59740800208ec8326ca1a
xyan0152 <xyan0152@student.monash.edu>
2025-04-03 21:16:48 +0800

    started react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Tailwind CSS test</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
