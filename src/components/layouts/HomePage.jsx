26b7c0c35bceba8ccfb1e526b4452b30582cd25b
xyan0152 <xyan0152@student.monash.edu>
2025-04-06 00:33:22 +0800

    finished homepage

--- src/components/layouts/HomePage.jsx
+++ src/components/layouts/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Map, Leaf, Home, MapPin, BookOpen, Users } from 'lucide-react';
 import klmap from '../../assets/homepage/kl-map.png';
 import heroImage from '../../assets/homepage/homepagehero55.jpg';
 
 const HomePage = () => {
+  // Scroll to top when the component mounts
+  useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
+
   return (
     <div className="flex flex-col">
       {/* Hero Section */}
