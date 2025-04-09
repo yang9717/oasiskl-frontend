8eca5277f751c0014d2f91b1f4cb57cb0f12dbd9
xyan0152 <xyan0152@student.monash.edu>
2025-04-09 18:19:10 +0800

    added notfound route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './components/layouts/HomePage';
 import GreenMap from './features/map/GreenMap';
 import GreenSpaceDetails from './features/greenSpaces/GreenSpaceDetails';
 import PlantDetails from './features/plants/PlantDetails';
+import NotFound from './components/layouts/NotFound';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/map" element={<GreenMap />} />
         <Route path="/spaces/:id" element={<GreenSpaceDetails />} />
         <Route path="/plants/:id" element={<PlantDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
