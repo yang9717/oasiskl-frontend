3ddfceb22e7b297e7a87298a5200b60ce1992edc
xyan0152 <xyan0152@student.monash.edu>
2025-04-19 01:58:27 +0800

    added epic3 routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import GreenMap from './features/map/GreenMap';
 import GreenSpaceDetails from './features/greenSpaces/GreenSpaceDetails';
 import PlantDetails from './features/plants/PlantDetails';
 import NotFound from './components/layouts/NotFound';
+import PlantGallery from './features/plants/PlantGallery';
+import PlantCareDetails from './features/plants/PlantCareDetails';
+import SoilGuide from './features/careGuides/SoilGuide';
+import HydroponicGuide from './features/careGuides/HydroponicGuide';
+import CareMain from './features/careGuides/CareMain';
+import PlantRecognition from './features/plantAI/PlantRecognition';
+import GeneralCare from './features/careGuides/GeneralCare';
 
 const App = () => {
   return (
@@ -15,6 +22,14 @@ const App = () => {
         <Route path="/map" element={<GreenMap />} />
         <Route path="/spaces/:id" element={<GreenSpaceDetails />} />
         <Route path="/plants/:id" element={<PlantDetails />} />
+        <Route path="/gallery" element={<PlantGallery />} />
+        <Route path="/care-guides" element={<CareMain />} />
+        <Route path="/care-guides/:id" element={<PlantCareDetails />} />
+        <Route path="/guides" element={<GeneralCare />} >
+          <Route path="potted-soil-plan" element={<SoilGuide />} />
+          <Route path="hydroponics-plan" element={<HydroponicGuide />} />
+        </Route>
+        <Route path="/plant-recognition" element={<PlantRecognition />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
