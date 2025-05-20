ad4a0d17e6efa1be22b65bbda8fa1bbb96deb8f4
xyan0152 <xyan0152@student.monash.edu>
2025-05-20 18:11:05 +0800

    removed mock contact info, adjusted the layout

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -5,9 +5,9 @@ import logoImage from '/assets/logo/logo-white.png';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-10">
-      <div className="max-w-6xl mx-auto px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="max-w-6xl mx-auto px-5 grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Column 1: Logo and tagline */}
-        <div>
+        <div className="md:col-span-1">
           <Link to="/" className="inline-block">
             <img 
               src={logoImage}
@@ -21,70 +21,55 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Column 2: Quick Links */}
-        <div>
+        {/* Column 2-3: Quick Links (spanning 2 columns) */}
+        <div className="md:col-span-2">
           <h3 className="text-lg font-bold mb-5 pb-2 relative">
             Quick Links
             <span className="absolute bottom-0 left-0 w-12 h-0.5 bg-green-500"></span>
           </h3>
-          <ul className="space-y-3">
-            <li>
-              <Link to="/" className="text-gray-300 hover:text-green-300 transition duration-200">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/map" className="text-gray-300 hover:text-green-300 transition duration-200">
-                Discover Green Space
-              </Link>
-            </li>
-            <li>
-              <Link to="/care-guides" className="text-gray-300 hover:text-green-300 transition duration-200">
-                Grow Your Green Corner
-              </Link>
-            </li>
-            <li>
-              <Link to="/gallery" className="text-gray-300 hover:text-green-300 transition duration-200">
-                Plant Gallery
-              </Link>
-            </li>
-            <li>
-              <Link to="/plant-recommender" className="text-gray-300 hover:text-green-300 transition duration-200">
-                Plant Recommender
-              </Link>
-            </li>
-            <li>
-              <Link to="/plant-identifier" className="text-gray-300 hover:text-green-300 transition duration-200">
-                Plant Identifier
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        {/* Column 3: Contact Us */}
-        <div>
-          <h3 className="text-lg font-bold mb-5 pb-2 relative">
-            Contact Us
-            <span className="absolute bottom-0 left-0 w-12 h-0.5 bg-green-500"></span>
-          </h3>
-          <ul className="space-y-3">
-            <li className="flex">
-              <span className="mr-3 text-green-400">📍</span>
-              <span className="text-gray-300">123 Green Street, Kuala Lumpur, Malaysia</span>
-            </li>
-            <li className="flex">
-              <span className="mr-3 text-green-400">✉️</span>
-              <a href="mailto:info@oasiskl.com" className="text-gray-300 hover:text-green-300 transition duration-200">
-                info@oasiskl.com
-              </a>
-            </li>
-            <li className="flex mt-4">
-              <span className="mr-3 text-green-400">📱</span>
-              <a href="tel:+60123456789" className="text-gray-300 hover:text-green-300 transition duration-200">
-                +60 12-345 6789
-              </a>
-            </li>
-          </ul>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {/* First column of links */}
+            <ul className="space-y-3">
+              <li>
+                <Link to="/" className="text-gray-300 hover:text-green-300 transition duration-200">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/map" className="text-gray-300 hover:text-green-300 transition duration-200">
+                  Discover Green Space
+                </Link>
+              </li>
+            </ul>
+            
+            {/* Second column of links */}
+            <ul className="space-y-3">
+              <li>
+                <Link to="/care-guides" className="text-gray-300 hover:text-green-300 transition duration-200">
+                  Grow Your Green Corner
+                </Link>
+              </li>
+              <li>
+                <Link to="/gallery" className="text-gray-300 hover:text-green-300 transition duration-200">
+                  Plant Gallery
+                </Link>
+              </li>
+            </ul>
+            
+            {/* Third column of links */}
+            <ul className="space-y-3">
+              <li>
+                <Link to="/plant-recommender" className="text-gray-300 hover:text-green-300 transition duration-200">
+                  Plant Recommender
+                </Link>
+              </li>
+              <li>
+                <Link to="/plant-identifier" className="text-gray-300 hover:text-green-300 transition duration-200">
+                  Plant Identifier
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       
